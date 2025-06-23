3c2d572538c60de81e48558386508985f0792b25
Atleastfivecats <levi.dye01@gmail.com>
2025-06-23 17:41:39 -0600

    Refactoring Split delim code

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -3,40 +3,22 @@ from textnode import TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes =[]
-    processed_nodes = ""
-    for node in old_nodes:
-        if text_type != "TextType.TEXT":
-            new_textnode = TextNode(node.text, text_type, node.url)
-            new_nodes.extend(new_textnode)
+    if old_nodes.TextType != TextType.TEXT:
+        new_nodes.append(old_nodes)
+
+    split_nodes = old_nodes.split(delimiter)
+    for node in split_nodes:
         
-        
-        if "```" in node.text:
-            deli_indicator = TextType.CODE
+'''
 
-        if "**" in node.text:
-            deli_indicator = TextType.BOLD
+If old is not textype.text extend it to the list as is
+Split the old nodes on the given delimiter
+If there is not a matching closing delimiter raise an exception
+Loop through the split nodes
+If a node matches the delimiter remove it
+Create a new textnode with the current node and add the correct textype to it
+Extend the newly made textnode to the list
+Return the list
 
-        if "__" in node.text:
-            deli_indicator = TextType.ITALIC
-
-        if "#" in node.text:
-            deli_indicator = TextType.HEADING
-
-        if "[]" in node.text:
-            deli_indicator = TextType.LINK
-
-        if "![]" in node.text:
-            deli_indicator = TextType.IMAGE
-
-        if "-" in node.text:
-            deli_indicator = TextType.UNORDERED_LIST
-
-        if "1." in node.text:
-            deli_indicator = TextType.ORDER_LIST
-
-        if ">" in node.text:
-            deli_indicator = TextType.QUOTE
-
-        split_node = node.text.split(delimiter)
-    
+'''
 
---
+++ src/temp_test.py
@@ -0,0 +1,5 @@
+test_str = "Abc def ghi . jkl mno"
+
+test_str.split(".")
+
+print(test_str)
