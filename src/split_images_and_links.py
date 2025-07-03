0a7de49398ec0a9a5fe56736c832d3cd8b3ac946
Atleastfivecats <levi.dye01@gmail.com>
2025-07-02 23:57:13 -0600

    started split images and links func

---
+++ src/split_images_and_links.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+from textnode import TextType
+from extract_links import extract_markdown_images
+from extract_links import extract_markdown_links
+from split_delimiter import split_nodes_delimiter
+
+def split_nodes_image(old_node):
+    
