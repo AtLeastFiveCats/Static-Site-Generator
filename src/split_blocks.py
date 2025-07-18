d8b445bb9d708334e78f8d03b7396edd649f8c61
Atleastfivecats <levi.dye01@gmail.com>
2025-07-18 00:06:17 -0600

    started markdown blocks

--- src/split_blocks.py
+++ src/split_blocks.py
@@ -1,3 +1,7 @@
 def markdown_block_split(markdown_text):
     split_text = markdown_text.split("\n\n")
-    #add split() function
+    stripped_text = split_text.strip()
+    for line in stripped_text:
+        if line == "":
+            stripped_text.del[line]
+    return stripped_text
