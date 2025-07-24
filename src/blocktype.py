a9eda3d2d9530547f5ee311aface9eb36f006df5
Atleastfivecats <levi.dye01@gmail.com>
2025-07-23 23:23:32 -0600

    working out the blocktypes

--- src/blocktype.py
+++ src/blocktype.py
@@ -9,32 +9,28 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered_list"
 
 def block_to_block_type(block):
-    expected_number = 1
-    hash_count = 0
+    if not block:
+        return BlockType.PARAGRAPH
 
-    for text in block:
-        for character in text:
-            if character == "#":
-                hash_count += 1
-            else:
-                continue
+    if block[0].startswith("```") and block[-1].endswith["```"]:
+        return BlockType.CODE
+    
+    beginning = block[0]
+    heading = ""
+    for i in range(6):
+        heading += "#"
+        if beginning.startswith(f"{heading} "):
+            return BlockType.PARAGRAPH
 
-        if len(text[:hash_count]) <= len(text) and hash_count <= 6:
-            if text[hash_count] + text[hash_count + 1] == "# ":
-                return BlockType.HEADING
+    if all(text.startswith("> ") for text in block):
+        return BlockType.QUOTE
 
-        elif text[:2] == "```" and text[-3::] == "```":
-            return BlockType.CODE
-
-        elif text[0] == ">":
-            return BlockType.QUOTE
-
-        elif text[:1] == "- ":
-            return BlockType.UNORDERED_LIST
-
-        elif text[:2] == f"{expected_number}. ":
+    elif all(text.startswith("- ") for text in block):
+        return BlockType.UNORDERED_LIST
+ 
+    elif text.startswith(f"{expected_number}. "):
             return BlockType.ORDERED_LIST
         
-        else:
-            return TextType.TEXT
+    else:
+        return BlockType.PARAGRAPH 
 
